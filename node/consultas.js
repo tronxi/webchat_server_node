049aba99bf83a054b4f694329d2ee1a9183dbd8c
tronxi <sergio.gsanchez97@gmail.com>
2019-01-04 17:06:56 +0100

    no insertar si usuario existe

--- node/consultas.js
+++ node/consultas.js
@@ -1,70 +1,67 @@
-var bd=require('./conexionBD');
+var bd = require('./conexionBD');
 var sha1 = require('sha1');
 
-exports.login = function(cb, usuario, pass)
-{
+exports.login = function (cb, usuario, pass) {
     let resultado = "";
     let qr = "select nombre, contra from usuario where nombre = '" + usuario + "'";
-    bd.query(qr, function(error, filas)
-    {
-        if(error)
-        {
+    bd.query(qr, function (error, filas) {
+        if (error) {
             console.log('error en el login');
             return;
         }
-        if(filas.length == 0)
-        {
+        if (filas.length == 0) {
             resultado = "noExiste";
         }
-        else if(filas[0].contra == sha1(pass))
-        {
+        else if (filas[0].contra == sha1(pass)) {
             resultado = "ok";
         }
-        else
-        {
+        else {
             resultado = "passIncorrecta";
         }
         cb(error, resultado);
     });
 }
 
-exports.registro = function(cb, usuario, pass)
+exports.registro = function (cb, usuario, pass) 
 {
     console.log(usuario);
     let resultado = "";
     let qr = "select nombre, contra from usuario where nombre = '" +
         usuario + "'";
-    bd.query(qr,function(error, filas)
+    bd.query(qr, function (error, filas) 
     {
-        if(error)
+        if (error) 
         {
             console.log('error al comprobar si existe usuario');
             return;
         }
-        if(filas.length > 0)
+        if (filas.length > 0) 
         {
             console.log("existe");
             resultado = "existe";
         }
-    });
-    if(resultado != "existe")
-    {
-        console.log("intento insertar");
-        qr = "insert into usuario (nombre, contra) values ('" +
-        usuario + "','" + sha1(pass) + "')";
-        bd.query(qr, function(error, filas)
+        else 
         {
-            if(error)
+            if (resultado != "existe") 
             {
-                console.log('error al introducir nuevo usuario');
-                return;
+                console.log("intento insertar");
+                qr = "insert into usuario (nombre, contra) values ('" +
+                    usuario + "','" + sha1(pass) + "')";
+                bd.query(qr, function (error, filas) 
+                {
+                    if (error) 
+                    {
+                        console.log('error al introducir nuevo usuario');
+                        return;
+                    }
+                    else 
+                    {
+                        resultado = "ok";
+                    }
+                });
             }
-            else
-            {
-                resultado = "ok";
-            }
-        });
-    }
-    cb(resultado);
+            cb(resultado);
+        }
+    });
 
 }
