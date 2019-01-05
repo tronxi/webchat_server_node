4d30de4d100ed2a40fa5db47cc05173b409401a8
tronxi <sergio.gsanchez97@gmail.com>
2019-01-05 13:23:13 +0100

    bug ;

--- node/consultas.js
+++ node/consultas.js
@@ -217,7 +217,7 @@ exports.registro = function (cb, usuario, pass) {
         iv = 'AAAAAAAAAAAAAAAA';
         var decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
         var buffer = decipher.update(data, 'base64');
-        buffer += decipher.final()
+        buffer += decipher.final();
         return buffer.toString();
         //return "hola";
       }
