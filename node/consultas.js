586a09436d27e3dfe8425228d679ff0c4943530b
tronxi <sergio.gsanchez97@gmail.com>
2019-01-05 18:08:26 +0100

    prueba

--- node/consultas.js
+++ node/consultas.js
@@ -219,9 +219,7 @@ exports.registro = function (cb, usuario, pass) {
         iv = 'AAAAAAAAAAAAAAAA';
         const algorithm = 'aes-256-cbc';
 
-        const decipher = crypto.createDecipheriv(algorithm, key, iv);
-        var decrypted = decipher.update(data,'binary','utf8');
-        decrypted += decipher.final('utf8');
-        var dec = new Buffer(decrypted, 'binary').toString('utf8');
-        return dec;
+        const decipher = crypto.createDecipher(algorithm, key);
+        var decrypted = decipher.update(data,'base64');
+        return decrypted;
       }
