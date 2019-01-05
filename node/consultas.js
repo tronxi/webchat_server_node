c89b386f4366f5b5ebe6203f5a9236c0acdfb102
tronxi <sergio.gsanchez97@gmail.com>
2019-01-05 13:36:49 +0100

    base64

--- node/consultas.js
+++ node/consultas.js
@@ -217,6 +217,6 @@ exports.registro = function (cb, usuario, pass) {
         key = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
         iv = 'AAAAAAAAAAAAAAAA';
         var decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-        return decipher.update(data, 'hex', 'utf8') + this.decipher.final('utf8');
+        return decipher.update(data, 'hex', 'base64') + this.decipher.final('base64');
         //return "hola";
       }
